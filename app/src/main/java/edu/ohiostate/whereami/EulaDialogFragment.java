445538ca58e247a37c993da015e0334bd5f72c66
acchampion <championone@gmail.com>
2019-10-28 16:38:11 -0400

    Migrated to AndroidX; added DayNight theme

--- app/src/main/java/edu/ohiostate/whereami/EulaDialogFragment.java
+++ app/src/main/java/edu/ohiostate/whereami/EulaDialogFragment.java
@@ -7,8 +7,9 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
 /**
  * Created by adamcchampion on 2014/09/22.
