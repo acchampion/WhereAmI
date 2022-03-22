ea76997f8243ce37bb9410aa979b5c493eacd4eb
Adam Champion <championone@gmail.com>
2022-03-22 19:55:48 -0400

    Updated Google Maps API renderer

---
+++ app/src/main/java/edu/ohiostate/whereami/MapsApplication.java
@@ -0,0 +1,34 @@
+package edu.ohiostate.whereami;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.MapsInitializer.Renderer;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
+
+
+public class MapsApplication extends Application implements OnMapsSdkInitializedCallback {
+
+	private final static String TAG = MapsApplication.class.getSimpleName();
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		MapsInitializer.initialize(getApplicationContext(), Renderer.LATEST, this);
+	}
+
+	@Override
+	public void onMapsSdkInitialized(@NonNull Renderer renderer) {
+		switch (renderer) {
+			case LATEST:
+				Log.d(TAG, "The latest version of the renderer is used.");
+				break;
+			case LEGACY:
+				Log.d(TAG, "The legacy version of the renderer is used.");
+				break;
+		}
+	}
+}
